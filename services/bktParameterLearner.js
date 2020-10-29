a0448425983d625316c8a76b79d70af588d16fa2
Doug Martin <dmartin@concord.org>
2020-10-29 11:01:35 -0400

    Change MONGODB_URI environment variable to DB_URI [#175291468]
    As part of the mLabs to Atlas migration the Mongo DB config var needs to be renamed from MONGODB_URI as it will be auto removed when the mLabs add-on is deleted.  After this removal the app will be restarted with the DB_URI set in the Heroku settings before the restart.

--- services/bktParameterLearner.js
+++ services/bktParameterLearner.js
@@ -7,7 +7,7 @@ const si = require('systeminformation');
 class BKTParameterLearner {
 	constructor() {
         this.conceptParameters = {};
-        this.client = null; 
+        this.client = null;
         this.perfStatsHeader = [];
         this.perfStatsValue = [];
         this.outputCsv = "";
@@ -20,26 +20,26 @@ class BKTParameterLearner {
         console.info("Start BKT parameter learner");
         this.startTime = new Date();
         this._recordPerformanceStats("StartTime", this.startTime.toISOString());
-        let url = new URL(process.env.MONGODB_URI);
+        let url = new URL(process.env.DB_URI);
         let dbName = url.pathname.substr(1);
         let db;
-        console.info("Connect to: %s db: %s", process.env.MONGODB_URI, dbName);
-        this._recordPerformanceStats("Database", process.env.MONGODB_URI);
-        return MongoClient.connect(process.env.MONGODB_URI).then((client) => {
+        console.info("Connect to: %s db: %s", process.env.DB_URI, dbName);
+        this._recordPerformanceStats("Database", process.env.DB_URI);
+        return MongoClient.connect(process.env.DB_URI).then((client) => {
             this.client = client;
             return this.client.db(dbName);
         }).then((dbConnection) => {
-            db = dbConnection; 
+            db = dbConnection;
             return db.collection('students').count();
-        }).then((totalStudents) => {       
-            this._recordPerformanceStats("TotalStudentsInDb", totalStudents);     
+        }).then((totalStudents) => {
+            this._recordPerformanceStats("TotalStudentsInDb", totalStudents);
             if (totalStudents === 0) {
                 throw new Error(`Database does not exist or is empty: ${dbName}`);
-            }      
+            }
             return db.collection('conceptobservations').count();
-        }).then((totalConceptObservations) => {       
-            this._recordPerformanceStats("TotalConceptObservationsInDb", totalConceptObservations);                                   
-            return this._queryStudents(studentIds, db);            
+        }).then((totalConceptObservations) => {
+            this._recordPerformanceStats("TotalConceptObservationsInDb", totalConceptObservations);
+            return this._queryStudents(studentIds, db);
         }).then((studentIdsFound) => {
             this._recordPerformanceStats("StudentIdsFromCSV", studentIds.length);
             this._recordPerformanceStats("StudentIdsFoundInDb", studentIdsFound.length);
@@ -47,11 +47,11 @@ class BKTParameterLearner {
         }).then((conceptObservations) => {
             this._recordPerformanceStats("ConceptObservationsFoundInDb", conceptObservations.length);
             let computeKTparamsAll = new ComputeKTparamsAll();
-            this.outputCsv = computeKTparamsAll.computeParameters(conceptObservations);    
+            this.outputCsv = computeKTparamsAll.computeParameters(conceptObservations);
             this.endTime = new Date();
             this._recordPerformanceStats("EndTime", this.endTime.toISOString());
             this._recordPerformanceStats("RunTime", this.runTimeInSeconds());
-            return this._createPerformanceStatsCSVAsync();        
+            return this._createPerformanceStatsCSVAsync();
         }).then((statsCsv) => {
             this.statsCsv = statsCsv;
             return this;
@@ -68,7 +68,7 @@ class BKTParameterLearner {
         }
 
         let end = this.endTime != null ? this.endTime : new Date();
-        return (end - this.startTime) / 1000; 
+        return (end - this.startTime) / 1000;
     }
 
     _recordPerformanceStats(header, value) {
@@ -76,7 +76,7 @@ class BKTParameterLearner {
         this.perfStatsValue.push(value);
         console.info("%s=%s", header, value);
     }
-    
+
     _createPerformanceStatsCSVAsync() {
         return si.cpu().then((cpuData) => {
             this._recordPerformanceStats("CpuType", `${cpuData.manufacturer} ${cpuData.brand}`);
@@ -92,7 +92,7 @@ class BKTParameterLearner {
             csv.push(this.perfStatsValue.join(","))
             return csv.join("\n");
         });
-	} 
+	}
 
     _queryStudents(studentIds, db) {
         let query = {};
@@ -100,7 +100,7 @@ class BKTParameterLearner {
 
         let displayableFilter = JSON.stringify(query, null, 2).substring(1, 256);
         //console.info("Query students | filter: " + displayableFilter);
-        
+
         return db.collection('students').distinct("id", query, {});
     }
 
@@ -114,7 +114,7 @@ class BKTParameterLearner {
 
         let displayableFilter = JSON.stringify(query, null, 2).substring(1, 256);
         //console.info("Query concept observations | filter: " + displayableFilter);
-        
+
         return db.collection('conceptobservations')
         .find(
             query,
